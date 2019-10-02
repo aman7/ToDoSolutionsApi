72195c01a2812cf7708be15bcc746c3193715efe
aman verma <error.boy@LIVE.COM>
2019-10-03 03:59:32 +0530

    app is using mssql now.

--- ToDoSolutionsApi/Startup.cs
+++ ToDoSolutionsApi/Startup.cs
@@ -29,7 +29,7 @@ namespace ToDoSolutionsApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddDbContext<ApplicationDBContext>(options => 
+            //services.AddDbContext<ApplicationDBContext>(options =>
             //options.UseNpgsql(
             //    Configuration.GetConnectionString("DefaultConnection")));
 
